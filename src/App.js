45a6a843cc2461ea7cf942c738a1807ab822e778
Andantez <kaloyanatanasov83@gmail.com>
2021-02-22 18:10:51 +0000

    Add state, form and submit func

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import Values from 'values.js';
 
 const App = () => {
+  const [colors, setColors] = useState('');
+  const [userInput, setUserInput] = useState('');
+  const [error, setError] = useState(false);
 
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!userInput) {
+      return;
+    }
+    try {
+      const colorsList = new Values(userInput).all(10);
+      console.log(colorsList)
+      setColors(colorsList);
+    } catch (error) {
+      setError(true);
+    }
+  };
   return (
-    <div>
-      <h1>Hello There</h1>
-    </div>
+    <main>
+      <h3>Color Shades Generator</h3>
+      <section>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="color">enter hex color</label>
+          <input
+            type="text"
+            id="color"
+            placeholder="#4287f5"
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </section>
+    </main>
   );
-}
+};
 
 export default App;
