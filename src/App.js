90ff29df8a74540ba4e0da86d8a8bd4036c37924
Andantez <kaloyanatanasov83@gmail.com>
2021-02-23 00:34:10 +0000

    Add - error show/hide animation, SingleColor component and styling, copy to clipboard on click

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Values from 'values.js';
 import SingleColor from './SingleColor';
@@ -7,21 +7,41 @@ const App = () => {
   const [colors, setColors] = useState(new Values('#4287f5').all(10));
   const [userInput, setUserInput] = useState('');
   const [error, setError] = useState(false);
-
-
+  // TODO - Find a proper variable name
+  const [randomC, setRandomC] = useState('');
+  // -----------------------------------
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!userInput) {
       return;
     }
     try {
+      setError(false);
       const colorsList = new Values(userInput).all(10);
-      console.log(colorsList)
+      console.log(colorsList);
       setColors(colorsList);
     } catch (error) {
       setError(true);
     }
   };
+
+  // TODO- Change handleSubmit to handle the random color generation
+  // if not to fix generateRandomColor to handle Errors
+  const generateRandomColor = () => {
+    const randomColor = `#${Math.floor(Math.random() * 2 ** 24)
+      .toString(16)
+      .padStart(6, '0')}`;
+    setRandomC(randomColor);
+    setColors(new Values(randomColor).all(10));
+  };
+
+  useEffect(() => {
+    const cleanUpFunc = setTimeout(() => {
+      setError(false);
+    }, 3000);
+
+    return () => clearTimeout(cleanUpFunc);
+  }, [error]);
   return (
     <Wrapper>
       <header>
@@ -31,8 +51,12 @@ const App = () => {
       </header>
       <section className="form-container">
         <form onSubmit={handleSubmit}>
+          {error && (
+            <p className="error fadeIn">Please Provide Valid Hex Color</p>
+          )}
           <label htmlFor="color">enter hex color</label>
           <input
+            className={`${error && 'show'}`}
             type="text"
             id="color"
             placeholder="#4287f5"
@@ -41,8 +65,16 @@ const App = () => {
           />
           <button type="submit">Submit</button>
         </form>
+        {/* Generates Random Coolor On Click
+            Functionality working
+            TODO- ADD styling 
+            --------------------
+            <button type="button" onClick={generateRandomColor}> Generate Random Color</button>
+            <h3>{randomC}</h3>
+            --------------------
+        */}
       </section>
-      <section>
+      <section className="container">
         {colors.map((color, index) => {
           return (
             <SingleColor {...color} key={index} index={index} hex={color.hex} />
@@ -86,18 +118,19 @@ const Wrapper = styled.main`
 
     form {
       display: flex;
+      position: relative;
       text-transform: capitalize;
-      font-size: 2rem;
       font-family: 'RocknRoll One', sans-serif;
 
       label {
         margin: 0 1rem;
         color: #080808;
+        font-size: 1.5rem;
       }
 
       input {
         min-width: 300px;
-        font-size: 1.2rem;
+        font-size: 1.1rem;
         padding-left: 0.5rem;
         border: 1px solid #a3a6a3;
         border-top-left-radius: 13px;
@@ -110,7 +143,9 @@ const Wrapper = styled.main`
           border: 2px solid #080808;
         }
       }
-
+      .show {
+        border: 2px solid #d3405c;
+      }
       button {
         margin-left: 0.2rem;
         width: 150px;
@@ -118,14 +153,53 @@ const Wrapper = styled.main`
         color: #fff;
         border-top-right-radius: 13px;
         border-bottom-right-radius: 13px;
-        font-size: 1.1rem;
+        font-size: 1rem;
         border: none;
         font-family: 'RocknRoll One', sans-serif;
         &:hover {
           background: #a32f7f;
         }
+        &:focus {
+          outline: none;
+          border: 2px solid #080808;
+        }
       }
     }
+  }
+
+  .error {
+    position: absolute;
+    left: 250px;
+    top: -40px;
+    font-family: 'RocknRoll One', sans-serif;
+    color: #d3405c;
+  }
+
+  .fadeIn {
+    animation: fadeInAnimation 0.5s, fadeOutAnimation 0.5s 2.7s;
+  }
+
+  @keyframes fadeInAnimation {
+    from {
+      transform: scale(0);
+    }
+    to {
+      transform: scale(1);
+    }
+  }
+  @keyframes fadeOutAnimation {
+    from {
+      transform: scale(1);
+    }
+    to {
+      transform: scale(0);
+    }
+  }
+  .container {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    grid-template-rows: repeat(auto-fit, 1fr);
+    margin-top: 3rem;
   }
 `;
 export default App;
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,17 +1,48 @@
-import { useState} from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 const SingleColor = ({ weight, hex, index, rgb }) => {
-  const [alert, setAlert] = useState(true);
+  const [alert, setAlert] = useState(false);
   const hexColor = `#${hex}`;
-  const rgbColor = rgb.join(',')
-  return (
-    <article style={{background: `rgb(${rgbColor})`}}>
-      <p>{weight}</p>
-      <p>{hexColor}</p>
-      {alert && <p>copied to clipboard</p> }
-    </article>
-  )
-}
+  const rgbColor = rgb.join(',');
 
+  const handleCopy = () => {
+    setAlert(true);
+    navigator.clipboard.writeText(hexColor);
+    console.log(hexColor);
+  };
+
+  useEffect(() => {
+    const clearAlert = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+
+    return () => clearTimeout(clearAlert);
+  }, [alert]);
+  return (
+    <Wrapper
+      onClick={handleCopy}
+      index={index}
+      style={{ background: `rgb(${rgbColor})` }}
+    >
+      <p>{weight}%</p>
+      <p>{hexColor}</p>
+      {alert && <p>copied to clipboard</p>}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.article`
+  /* Adapt the color based on index prop for better Visability and Readability  */
+
+  height: 200px;
+  padding: 1rem 2rem;
+  font-family: 'RocknRoll One', sans-serif;
+  color: ${(props) => props.index > 10 && '#fff'};
+
+  p {
+    margin-bottom: 10px;
+    letter-spacing: 1.5px;
+  }
+`;
 export default SingleColor;
