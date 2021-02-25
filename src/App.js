8b852a9392f4fd3825df5b50ef59cfb2c4aff901
Andantez <kaloyanatanasov83@gmail.com>
2021-02-25 21:52:07 +0000

    Add: Form Component, Move: Form into its own component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Values from 'values.js';
 import SingleColor from './SingleColor';
-import { fadeIn, fadeOut } from './Animation';
 import { FaGithub } from 'react-icons/fa';
+import Form from './Form';
 
 const App = () => {
   const [colors, setColors] = useState(new Values('#de5648').all(10));
@@ -71,46 +71,16 @@ const App = () => {
           <a href="/">Tint & Shade Generator</a>
         </h1>
       </header>
-      <section className="form-container">
-        <form onSubmit={handleSubmit}>
-          {error && (
-            <p className="error fadeIn">Please Provide Valid Hex Color</p>
-          )}
-          <label htmlFor="color">enter hex color</label>
-          <input
-            className={`user-input ${error && 'show'}`}
-            type="text"
-            id="color"
-            placeholder={mainColor}
-            value={userInput}
-            onChange={(e) => setUserInput(e.target.value)}
-          />
-          <button type="submit" className="btn">
-            Submit
-          </button>
-          <button
-            type="button"
-            onClick={generateRandomColor}
-            className="random-btn"
-          >
-            Generate Random Color
-          </button>
-          <div className="form-control">
-            <h4>Color Weight</h4>
-            <div>
-              <input
-                className="range-input"
-                type="range"
-                min="2"
-                max="50"
-                value={colorWeight}
-                onChange={handleColorRange}
-              />
-              <span className="range-value">{colorWeight}%</span>
-            </div>
-          </div>
-        </form>
-      </section>
+      <Form
+        handleSubmit={handleSubmit}
+        error={error}
+        mainColor={mainColor}
+        userInput={userInput}
+        setUserInput={setUserInput}
+        generateRandomColor={generateRandomColor}
+        colorWeight={colorWeight}
+        handleColorRange={handleColorRange}
+      />
       <section className="container">
         {colors.map((color, index) => {
           return <SingleColor {...color} key={index} hex={color.hex} />;
@@ -149,157 +119,7 @@ const Wrapper = styled.main`
       color: #fff;
     }
   }
-  .form-container {
-    display: grid;
-    margin-top: 3rem;
 
-    form {
-      display: grid;
-      gap: 20px;
-      grid-template-columns: 1fr 1fr;
-      grid-template-areas:
-        'label label'
-        'input input'
-        'submitBtn randomBtn'
-        'range range';
-      margin: 0 auto;
-      position: relative;
-      text-transform: capitalize;
-      font-family: 'RocknRoll One', sans-serif;
-      text-align: center;
-      label {
-        margin: 0 1rem;
-        color: #080808;
-        font-size: 1.5rem;
-        grid-area: label;
-        /* position: relative; */
-      }
-
-      .user-input {
-        grid-area: input;
-        min-width: 300px;
-        font-size: 1.1rem;
-        padding-left: 0.5rem;
-        border: 1px solid #a3a6a3;
-        border-radius: 5px;
-        font-family: 'RocknRoll One', sans-serif;
-        &:focus {
-          outline: none;
-          border-radius: 5px;
-          border: 2px solid #080808;
-        }
-      }
-      .show {
-        border: 2px solid #d3405c;
-      }
-    }
-  }
-  .btn,
-  .random-btn {
-    margin-left: 0.2rem;
-    width: 150px;
-    background: #080808;
-    color: #fff;
-    border-radius: 5px;
-    font-size: 0.8rem;
-    border: none;
-    font-family: 'RocknRoll One', sans-serif;
-    border: 2px solid #080808;
-    padding: 10px 10px;
-    &:hover {
-      background: #a32f7f;
-      cursor: pointer;
-    }
-    &:focus {
-      outline: none;
-      border: 2px solid #080808;
-    }
-  }
-  .random-btn {
-    width: fit-content;
-
-    grid-area: randomBtn;
-  }
-  .btn {
-    grid-area: submitBtn;
-    min-width: 100%;
-  }
-  .btn:active,
-  .random-btn:active {
-    box-shadow: inset -4px -4px 8px rgba(182, 152, 174, 0.5),
-      inset 8px 8px 16px rgba(0, 0, 0, 0.1);
-  }
-  .form-control {
-    grid-area: range;
-    max-width: 100%;
-
-    h4 {
-      font-size: 1.2rem;
-      margin: 0.7rem 0;
-      color: #080808;
-    }
-    div {
-      display: flex;
-    }
-
-    .range-input {
-      flex-grow: 1;
-
-      &:focus {
-        outline: none;
-      }
-    }
-    /* Input - Range styles */
-    input[type='range'] {
-      -webkit-appearance: none;
-      width: 100%;
-      background: transparent;
-    }
-    input[type='range']::-webkit-slider-thumb {
-      -webkit-appearance: none;
-      border: 1px solid #080808;
-      height: 36px;
-      width: 36px;
-      border-radius: 50%;
-      background: ${(props) => props.mainColor};
-      cursor: pointer;
-
-      margin-top: -14px;
-      box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
-    }
-
-    input[type='range']::-webkit-slider-runnable-track {
-      width: 100%;
-      height: 8.4px;
-      cursor: pointer;
-      box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
-      background: #080808;
-      border-radius: 5px;
-      border: 0.2px solid #010101;
-    }
-    input[type='range']:focus::-webkit-slider-runnable-track {
-      background: #080808;
-    }
-    /* ---------------------------- */
-    .range-value {
-      background: #fff;
-      padding: 10px;
-      border-radius: 5px;
-      margin-left: 10px;
-      min-width: 60px;
-      border: 2px solid #080808;
-    }
-  }
-  .error {
-    position: absolute;
-    left: 18%;
-    top: -40px;
-    font-family: 'RocknRoll One', sans-serif;
-    color: #d3405c;
-  }
-  .fadeIn {
-    animation: ${fadeIn} 0.5s, ${fadeOut} 0.5s 2.7s;
-  }
   .container {
     display: grid;
     min-height: calc(100vh - 100px);
@@ -391,7 +211,7 @@ const Wrapper = styled.main`
     }
     .btn,
     .random-btn {
-      padding: 0px 10px;
+      padding: 5px 10px;
     }
   }
   @media screen and (max-width: 320px) {
---
+++ src/Form.js
@@ -0,0 +1,211 @@
+import styled from 'styled-components';
+import { fadeIn, fadeOut } from './Animation';
+
+const Form = ({
+  handleSubmit,
+  error,
+  mainColor,
+  userInput,
+  setUserInput,
+  generateRandomColor,
+  colorWeight,
+  handleColorRange,
+}) => {
+  return (
+    <Wrapper className="form-container" mainColor={mainColor}>
+      <form onSubmit={handleSubmit}>
+        {error && (
+          <p className="error fadeIn">Please Provide Valid Hex Color</p>
+        )}
+        <label htmlFor="color">enter hex color</label>
+        <input
+          className={`user-input ${error && 'show'}`}
+          type="text"
+          id="color"
+          placeholder={mainColor}
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Submit
+        </button>
+        <button
+          type="button"
+          onClick={generateRandomColor}
+          className="random-btn"
+        >
+          Generate Random Color
+        </button>
+        <div className="form-control">
+          <h4>Color Weight</h4>
+          <div>
+            <input
+              className="range-input"
+              type="range"
+              min="2"
+              max="50"
+              value={colorWeight}
+              onChange={handleColorRange}
+            />
+            <span className="range-value">{colorWeight}%</span>
+          </div>
+        </div>
+      </form>
+    </Wrapper>
+  );
+};
+
+export default Form;
+
+const Wrapper = styled.section`
+  display: grid;
+  margin-top: 3rem;
+
+  form {
+    display: grid;
+    gap: 20px;
+    grid-template-columns: 1fr 1fr;
+    grid-template-areas:
+      'label label'
+      'input input'
+      'submitBtn randomBtn'
+      'range range';
+    margin: 0 auto;
+    position: relative;
+    text-transform: capitalize;
+    font-family: 'RocknRoll One', sans-serif;
+    text-align: center;
+    label {
+      margin: 0 1rem;
+      color: #080808;
+      font-size: 1.5rem;
+      grid-area: label;
+      /* position: relative; */
+    }
+
+    .user-input {
+      grid-area: input;
+      min-width: 300px;
+      font-size: 1.1rem;
+      padding-left: 0.5rem;
+      border: 1px solid #a3a6a3;
+      border-radius: 5px;
+      font-family: 'RocknRoll One', sans-serif;
+      &:focus {
+        outline: none;
+        border-radius: 5px;
+        border: 2px solid #080808;
+      }
+    }
+    .show {
+      border: 2px solid #d3405c;
+    }
+  }
+
+  .btn,
+  .random-btn {
+    /* margin-left: 0.2rem; */
+    width: 150px;
+    background: #080808;
+    color: #fff;
+    border-radius: 5px;
+    font-size: 0.8rem;
+    border: none;
+    font-family: 'RocknRoll One', sans-serif;
+    border: 2px solid #080808;
+    padding: 10px 10px;
+    &:hover {
+      background: #a32f7f;
+      cursor: pointer;
+    }
+    &:focus {
+      outline: none;
+      border: 2px solid #080808;
+    }
+  }
+  .random-btn {
+    width: fit-content;
+
+    grid-area: randomBtn;
+  }
+  .btn {
+    grid-area: submitBtn;
+    min-width: 100%;
+  }
+  .btn:active,
+  .random-btn:active {
+    box-shadow: inset -4px -4px 8px rgba(182, 152, 174, 0.5),
+      inset 8px 8px 16px rgba(0, 0, 0, 0.1);
+  }
+  .form-control {
+    grid-area: range;
+    max-width: 100%;
+
+    h4 {
+      font-size: 1.2rem;
+      margin: 0.7rem 0;
+      color: #080808;
+    }
+    div {
+      display: flex;
+    }
+
+    .range-input {
+      flex-grow: 1;
+
+      &:focus {
+        outline: none;
+      }
+    }
+    /* Input - Range styles */
+    input[type='range'] {
+      -webkit-appearance: none;
+      width: 100%;
+      background: transparent;
+    }
+    input[type='range']::-webkit-slider-thumb {
+      -webkit-appearance: none;
+      border: 1px solid #080808;
+      height: 36px;
+      width: 36px;
+      border-radius: 50%;
+      background: ${(props) => props.mainColor};
+      cursor: pointer;
+
+      margin-top: -14px;
+      box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    }
+
+    input[type='range']::-webkit-slider-runnable-track {
+      width: 100%;
+      height: 8.4px;
+      cursor: pointer;
+      box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+      background: #080808;
+      border-radius: 5px;
+      border: 0.2px solid #010101;
+    }
+    input[type='range']:focus::-webkit-slider-runnable-track {
+      background: #080808;
+    }
+    /* ---------------------------- */
+    .range-value {
+      background: #fff;
+      padding: 10px;
+      border-radius: 5px;
+      margin-left: 10px;
+      min-width: 60px;
+      border: 2px solid #080808;
+    }
+  }
+  .error {
+    position: absolute;
+    left: 18%;
+    top: -40px;
+    font-family: 'RocknRoll One', sans-serif;
+    color: #d3405c;
+  }
+  .fadeIn {
+    animation: ${fadeIn} 0.5s, ${fadeOut} 0.5s 2.7s;
+  }
+`;
